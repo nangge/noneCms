b5947dbf8b23759f013532f8aedbbfdf90620efe
553212320@qq.com <553212320@qq.com>
2017-03-20 16:26:47 +0800

    安装文件修改

--- public/install/tpl/step_5.php
+++ public/install/tpl/step_5.php
@@ -2,7 +2,7 @@
 	<div class="main" style="background: rgba(255, 255, 255, 0.35);">
 		<div class="complete">
 				<p style="font-family: microsoft yahei,simhei; font-size:20px; font-weight:bold; color:#FF3300"><?php echo $lang['congratulations_installation_success'];?></p>
-				<p><a href="../" target="_blank"  class="btn_blue"><?php echo $lang['visit_home'];?></a><a href="../admin" target="_blank" class="btn_blue"><?php echo $lang['enter_admin'];?></a></p>
+				<p><a href="../" target="_blank"  class="btn_blue"><?php echo $lang['visit_home'];?></a><a href="../index.php/admin" target="_blank" class="btn_blue"><?php echo $lang['enter_admin'];?></a></p>
 				<p><?php echo $lang['safe_notes'];?></p>
 		
 		</div>
